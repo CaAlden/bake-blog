ae7784e18481cfe9353639ac7dce0adc8308f5fd
Campbell Alden <campbell.alden@bibo.com.ph>
2021-02-07 14:20:51 +0900

    Add type declarations for image file types

---
+++ image.d.ts
@@ -0,0 +1,12 @@
+declare module '*.png' {
+  const img: string;
+  export default img;
+}
+declare module '*.webp' {
+  const img: string;
+  export default img;
+}
+declare module '*.jpeg' {
+  const img: string;
+  export default img;
+}
