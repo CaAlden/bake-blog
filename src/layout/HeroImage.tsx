f1c06d215cfdf6717f227d0305b98f6df4f977fd
Campbell Alden <campbell.alden@bibo.com.ph>
2021-03-14 19:34:10 +0900

    Fix ios images

--- src/layout/HeroImage.tsx
+++ src/layout/HeroImage.tsx
@@ -1,18 +1,21 @@
 import * as React from 'react';
 import { css } from '@emotion/css';
 import { Image } from '../../types';
+import { Breakpoint, useBreakpoint } from '../context';
 
 interface IProps {
   image: Image;
   text?: string;
 }
 
-const getHeroImageClass = (image: Image) => css({
-  backgroundImage: `url(${image.large ?? image.large})`,
+const heroImageClass = css({
   backgroundRepeat: 'no-repeat',
   backgroundAttachment: 'fixed',
   backgroundPosition: 'center',
-  backgroundSize: 'cover', /* Resize the background image to cover the entire container */
+  backgroundSize: 'cover',
+  '-moz-background-size': 'cover',
+  '-webkit-background-size': 'cover',
+  '-o-background-size': 'cover',
 
   display: 'flex',
   justifyContent: 'center',
@@ -29,8 +32,12 @@ const textClass = css({
   margin: 0,
 });
 export default function HeroImage(props: IProps) {
+  const breakpoint = useBreakpoint();
   return (
-    <div className={getHeroImageClass(props.image)}>
+    <div className={heroImageClass} style={{
+      backgroundImage: `url(${props.image.large})`,
+      backgroundAttachment:  breakpoint !== Breakpoint.Large ? 'scroll' : undefined,
+    }}>
     {props.text &&
       <h1 className={textClass}>{props.text}</h1>
     }</div>
