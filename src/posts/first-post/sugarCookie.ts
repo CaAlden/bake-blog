82ef1be6b14ef07d0b77d5eed8a25e819f5497a6
Campbell Alden <campbell.alden@bibo.com.ph>
2021-07-22 15:30:52 +0900

    Add correct amounts to the Sugar Cookie Recipe

--- src/posts/first-post/sugarCookie.ts
+++ src/posts/first-post/sugarCookie.ts
@@ -1,8 +1,9 @@
-import { Units, Recipe, Difficulty, Image, IRecipeDetail } from "../../../types";
-import { getAllPurposeFlour, getButter, getEggs, getEggWhite, getSugar, getYolk } from "../../utils/ingredients";
+import { Units, Recipe, Difficulty, Image, IRecipeDetail, Ingredient } from "../../../types";
+import { getAllPurposeFlour, getBakingSoda, getButter, getEggs, getEggWhite, getSalt, getSugar, getYolk, makeIngredient } from "../../utils/ingredients";
 import HeroLarge from "../../../assets/img/placeholder_large.jpeg";
 import HeroMedium from "../../../assets/img/placeholder_medium.jpeg";
 import HeroSmall from "../../../assets/img/placeholder_small.jpeg";
+import { EXTRAS_COLOR } from "../../utils/Colors";
 
 const heroImage: Image = {
   large: HeroLarge,
@@ -13,7 +14,9 @@ const heroImage: Image = {
 const STEPS = `
 ## Sugar Cookie Core Recipe
 
-1. Mix together the **Sugar** and **Butter** until they are smoothly encorperated. Typically it is easier to achieve a smoother texture if the **Butter** is at room temperature when you combine it.
+Yield: 24 cookies.
+
+1. Mix together the **Sugar**, **Salt** and **Butter** until they are smoothly encorperated. Typically it is easier to achieve a smoother texture if the **Butter** is at room temperature when you combine it.
 2. Add the **Vanilla Extract** and **Eggs** to the sugar-butter mixture.
 3. Finally add the **Flour** and **Baking Soda** and mix until the dough has come together.
 4. Roll the dough out on a lightly floured work surface until it is thin (about [1/2 - 3/4 cm|1/4 - 1/3 in](#units)). Place the rolled out dough onto a **baking sheet** so that it can be transfered into a refridgerator to cool. When we did [the experiment](/posts/first-post) we let it cool for **1 hour**, but it can be cooled for multiple days if you are making the dough ahead of time.
@@ -21,16 +24,68 @@ const STEPS = `
 6. Bake the cookies for **10 - 12** minutes.
 `;
 
+const SHARED_INGREDIENTS: Ingredient[] = [
+  makeIngredient('Vanilla Extract', '2tsp (7 g)', '2 tsp', EXTRAS_COLOR, []),
+  getEggs(1),
+  getBakingSoda(3.5),
+  getSalt(2.5),
+];
+
 const ingredientMap = {
-  'Low-Low': [],
-  'Low-Medium': [],
-  'Low-High': [],
-  'Medium-Low': [],
-  'Medium-Medium': [],
-  'Medium-High': [],
-  'High-Low': [],
-  'High-Medium': [],
-  'High-High': [],
+  'Low-Low': [
+    getAllPurposeFlour(250),
+    getSugar(150),
+    getButter(300, false),
+    ...SHARED_INGREDIENTS,
+  ],
+  'Low-Medium': [
+    getAllPurposeFlour(250),
+    getSugar(200),
+    getButter(250, false),
+    ...SHARED_INGREDIENTS,
+  ],
+  'Low-High': [
+    getAllPurposeFlour(250),
+    getSugar(225),
+    getButter(230, false),
+    ...SHARED_INGREDIENTS,
+  ],
+  'Medium-Low': [
+    getAllPurposeFlour(310),
+    getSugar(150),
+    getButter(250, false),
+    ...SHARED_INGREDIENTS,
+  ],
+  'Medium-Medium': [
+    getAllPurposeFlour(310),
+    getSugar(180),
+    getButter(200, false),
+    ...SHARED_INGREDIENTS,
+  ],
+  'Medium-High': [
+    getAllPurposeFlour(310),
+    getSugar(200),
+    getButter(175, false),
+    ...SHARED_INGREDIENTS,
+  ],
+  'High-Low': [
+    getAllPurposeFlour(360),
+    getSugar(225),
+    getButter(200, false),
+    ...SHARED_INGREDIENTS,
+  ],
+  'High-Medium': [
+    getAllPurposeFlour(360),
+    getSugar(200),
+    getButter(160, false),
+    ...SHARED_INGREDIENTS,
+  ],
+  'High-High': [
+    getAllPurposeFlour(360),
+    getSugar(225),
+    getButter(125, false),
+    ...SHARED_INGREDIENTS,
+  ],
 }
 
 const getDetail = (key: keyof typeof ingredientMap):[string, IRecipeDetail] => [
